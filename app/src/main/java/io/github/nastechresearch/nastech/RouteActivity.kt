f997e6aa9f68171932131650e59f9ceeb0e054fa
rey99913999 <bucket2011water@gmail.com>
2026-09-18 11:12:18 +0300

    Update 02: add Tasks navigation route

--- app/src/main/java/io/github/nastechresearch/nastech/RouteActivity.kt
+++ app/src/main/java/io/github/nastechresearch/nastech/RouteActivity.kt
@@ -151,6 +151,7 @@ import io.github.nastechresearch.nastech.ui.pages.setting.SettingTelegramPage
 import io.github.nastechresearch.nastech.ui.pages.setting.SettingWebPage
 import io.github.nastechresearch.nastech.ui.pages.share.handler.ShareHandlerPage
 import io.github.nastechresearch.nastech.ui.pages.stats.StatsPage
+import io.github.nastechresearch.nastech.ui.pages.tasks.TasksPage
 import io.github.nastechresearch.nastech.ui.pages.translator.TranslatorPage
 import io.github.nastechresearch.nastech.ui.pages.webview.WebViewPage
 import io.github.nastechresearch.nastech.ui.pages.welcome.NastechWelcomeOverlay
@@ -601,6 +602,10 @@ class RouteActivity : ComponentActivity() {
                                 io.github.nastechresearch.nastech.ui.pages.setting.scheduledjobs.ScheduledJobsScreen()
                             }
 
+                            entry<Screen.Tasks> {
+                                TasksPage()
+                            }
+
                             entry<Screen.SettingBrowser> {
                                 io.github.nastechresearch.nastech.ui.pages.setting.browser.SettingBrowserPage()
                             }
@@ -909,6 +914,9 @@ sealed interface Screen : NavKey {
 
     @Serializable
     data object SettingScheduledJobs : Screen
+
+    @Serializable
+    data object Tasks : Screen
 
     @Serializable
     data object SettingBrowser : Screen
