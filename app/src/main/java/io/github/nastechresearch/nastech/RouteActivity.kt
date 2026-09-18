f05eda1de6f9fdd5f4efa9de6eb0c3eb66f90a4f
rey99913999 <bucket2011water@gmail.com>
2026-09-18 12:34:11 +0300

    Fix task detail navigation route

--- app/src/main/java/io/github/nastechresearch/nastech/RouteActivity.kt
+++ app/src/main/java/io/github/nastechresearch/nastech/RouteActivity.kt
@@ -151,6 +151,7 @@ import io.github.nastechresearch.nastech.ui.pages.setting.SettingTelegramPage
 import io.github.nastechresearch.nastech.ui.pages.setting.SettingWebPage
 import io.github.nastechresearch.nastech.ui.pages.share.handler.ShareHandlerPage
 import io.github.nastechresearch.nastech.ui.pages.stats.StatsPage
+import io.github.nastechresearch.nastech.ui.pages.tasks.TaskDetailPage
 import io.github.nastechresearch.nastech.ui.pages.tasks.TasksPage
 import io.github.nastechresearch.nastech.ui.pages.translator.TranslatorPage
 import io.github.nastechresearch.nastech.ui.pages.webview.WebViewPage
@@ -606,6 +607,10 @@ class RouteActivity : ComponentActivity() {
                                 TasksPage()
                             }
 
+                            entry<Screen.TaskDetail> { key ->
+                                TaskDetailPage(taskId = key.id)
+                            }
+
                             entry<Screen.SettingBrowser> {
                                 io.github.nastechresearch.nastech.ui.pages.setting.browser.SettingBrowserPage()
                             }
@@ -917,6 +922,9 @@ sealed interface Screen : NavKey {
 
     @Serializable
     data object Tasks : Screen
+
+    @Serializable
+    data class TaskDetail(val id: String) : Screen
 
     @Serializable
     data object SettingBrowser : Screen
