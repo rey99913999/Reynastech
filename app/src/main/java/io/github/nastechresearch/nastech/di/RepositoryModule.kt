7316c63eca05d2983210c1abf570b49323fc53b1
rey99913999 <bucket2011water@gmail.com>
2026-09-18 23:58:49 +0300

    Update 04: register agent configuration persistence

--- app/src/main/java/io/github/nastechresearch/nastech/di/RepositoryModule.kt
+++ app/src/main/java/io/github/nastechresearch/nastech/di/RepositoryModule.kt
@@ -8,6 +8,7 @@ import io.github.nastechresearch.nastech.data.memory.ConversationMemoryEngine
 import io.github.nastechresearch.nastech.data.task.TaskManager
 import io.github.nastechresearch.nastech.data.execution.ExecutionTelemetry
 import io.github.nastechresearch.nastech.data.execution.LocalExecutionEngine
+import io.github.nastechresearch.nastech.data.agentconfig.ConversationAgentConfigRepository
 import io.github.nastechresearch.nastech.data.repository.ConversationRepository
 import io.github.nastechresearch.nastech.data.repository.FavoriteRepository
 import io.github.nastechresearch.nastech.data.repository.FolderRepository
@@ -46,6 +47,8 @@ val repositoryModule = module {
     single { ExecutionTelemetry(get()) }
 
     single { LocalExecutionEngine(taskManager = get(), telemetry = get()) }
+
+    single { ConversationAgentConfigRepository(database = get(), memoryEngine = get(), taskManager = get()) }
 
     single {
         GenMediaRepository(get())
