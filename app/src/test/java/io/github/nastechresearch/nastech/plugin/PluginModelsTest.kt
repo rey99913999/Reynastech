b381dc2df002bb409f4c3a3b1c47f009f42c1e7e
rey99913999 <bucket2011water@gmail.com>
2026-09-26 13:01:11 +0300

    Fix stale JVM unit test APIs

--- app/src/test/java/io/github/nastechresearch/nastech/plugin/PluginModelsTest.kt
+++ app/src/test/java/io/github/nastechresearch/nastech/plugin/PluginModelsTest.kt
@@ -1,8 +1,8 @@
 package io.github.nastechresearch.nastech.plugin
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
 
 class PluginModelsTest {
     @Test
